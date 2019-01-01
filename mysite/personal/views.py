6f6976879a78190762710944e074edcd0b0269c3
Singhbalbir <balbir4email@gmail.com>
2019-01-01 14:41:00 +0530

    Added html snippet

---
+++ mysite/personal/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'personal/home.html')
+
+# You can use 'home.html' also but if project is big them 
+# there will be lot of templates and we might end up with 
+# more than one template with same name.
+# To avoid that mkdir inside templates dir.
