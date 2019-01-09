fe16f395728c02e2d923077405e585bf193ac41b
Balbir Singh <balbir4email@gmail.com>
2019-01-09 23:56:12 +0530

    added comments

--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'personal/home.html')
 
-    # You can use 'home.html' also but if project is big them 
+    # You can use 'home.html' also but if project is big then 
     # there will be lot of templates and we might end up with 
     # more than one template with same name.
-    # To avoid that mkdir inside templates dir.
+    # To avoid that make new directory inside templates directory.
 
 def contact(request):
     return render(request, 'personal/contact.html')
